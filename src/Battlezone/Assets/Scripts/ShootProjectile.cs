6d912a7633a739bc44dda61fb54e3f28be73fee8
Jake Mulholland <eckojay2192@gmail.com>
2015-03-17 18:16:55 +0000

    Added shooting script to project
    -Added shooting script to camera (Shoot with mouse1)
    -Increased the size of the plane
    -Added two enemy gameobjects
    -Added tank shell to resources
    -Added enemy spawner script (Not fully working yet)
    -Moved default camera position so it no longer spawn beneath the plane

---
+++ src/Battlezone/Assets/Scripts/ShootProjectile.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ShootProjectile : MonoBehaviour {
+	
+	GameObject prefab;
+	
+	// Use this for initialization
+	void Start () {
+		prefab = Resources.Load ("TankShell") as GameObject;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetMouseButtonDown (0)) {
+			GameObject TankShell = Instantiate (prefab) as GameObject;
+			TankShell.transform.position = transform.position+Camera.main.transform.forward;
+			Rigidbody rb = TankShell.GetComponent<Rigidbody>();
+			rb.velocity = Camera.main.transform.forward * 40;
+		}
+	}
+}
---
+++ src/Battlezone/Assets/Scripts/SpawnManager.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SpawnManager : MonoBehaviour {
+
+	public GameObject Enemy1;
+
+	// Use this for initialization
+	void Start () {
+		StartCoroutine (IEnumerator ());
+	}
+	
+	IEnumerator SpawnEnemny(){
+		while (true) {
+			//Object to spawn, Initial Vector3 position, no rotation
+			Instantiate(Enemy1, Transform.position, Quaternion.identity);
+			//Wait time for IEnumerator to be called. Better on CPU overhead
+			yield return new WaitForSeconds(5f);
+		}
+	}
+}
