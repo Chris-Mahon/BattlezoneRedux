162104ebbef9fb1bac909befb3dd0e9f6a54c335
Christopher Mahon <christopher.mahon2@student.dit.ie>
2015-04-19 01:48:25 +0100

    Updated with not working fire mechanism
    Currently does not shoot when within a certain range, however the
    general idea is there

--- src/Battlezone/Assets/Scripts/EnemyAI.cs
+++ src/Battlezone/Assets/Scripts/EnemyAI.cs
@@ -9,15 +9,27 @@ public class EnemyAI : MonoBehaviour {
 		Vector3 PlayerLocale = GameObject.Find("Player").transform.position;
 		print (PlayerLocale.x);
 		transform.LookAt(PlayerLocale);
+		float speed = 0.0f;
 	}
 	
 	// Update is called once per frame
 	void Update () 
 	{
-
 		Vector3 PlayerLocale = GameObject.Find("Player").transform.position;
 		transform.LookAt(PlayerLocale);
-		transform.position = Vector3.MoveTowards(transform.position, PlayerLocale, 0.5f*Time.deltaTime);
+		float distance = Vector3.Distance (GameObject.Find("Player").transform.position, gameObject.transform.position);
+		print ("Distance is", distance);
 
+		if (distance > 10) 
+		{
+			speed = 0.0f;
+			transform.position = Vector3.MoveTowards(transform.position, PlayerLocale, speed * Time.deltaTime);
+		}
+		else 
+		{
+			WaitForSeconds(5);
+			print("Hi");
+			speed = 0.5f;
+		}
 	}
 }
